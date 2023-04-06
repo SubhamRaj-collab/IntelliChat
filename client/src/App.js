3d3b7c797684ab7d5578aab47c3b575a52737e5a
Subham Raj <glorious10info@gmail.com>
2023-04-06 17:58:52 +0530

    User-side Query Interface Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
-import 'normalize.css';
+import './normalize.css'
 
 function App() {
   return (
     <div className="App">
       <aside className="sideMenu">
-        <h1>Aside</h1>
+        <div className="sideMenuButton">
+          <span>+</span>
+          New Chat
+        </div>
       </aside>
       
       <section className="chatBox">
-        
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar">
+                
+                </div>
+                <div className="message">
+                  Hello World
+                </div>
+              </div>
+          </div> 
+        </div>
+        <div className="chat-input-holder">
+          <textarea className="chat-input-textarea" rows='1' placeholder="Send a message..."></textarea>
+        </div>
       </section>
       
     </div>
