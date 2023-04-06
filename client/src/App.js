db45b1658fc13349973d853637cf24f244e75a7f
Subham Raj <glorious10info@gmail.com>
2023-04-06 15:35:30 +0530

    client subfolder added in Intelli-chat

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import 'normalize.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sideMenu">
+        <h1>Aside</h1>
+      </aside>
+      
+      <section className="chatBox">
+        
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
